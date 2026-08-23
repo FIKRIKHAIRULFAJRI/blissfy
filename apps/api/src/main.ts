b31d1faa515434452ac311261d67f5c576f887fa
FIKRIKHAIRULFAJRI <fikrikhairul354@gmail.com>
2026-08-23 23:25:50 +0700

    feat(api): configure validation and CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -6,7 +7,29 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+
   const port = configService.get<number>('PORT', 3002);
+  const storeUrl = configService.get<string>(
+    'STORE_URL',
+    'http://localhost:3000',
+  );
+  const adminUrl = configService.get<string>(
+    'ADMIN_URL',
+    'http://localhost:3001',
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: [storeUrl, adminUrl],
+    credentials: true,
+  });
 
   await app.listen(port);
 }
